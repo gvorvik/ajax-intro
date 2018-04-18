784c2a663c6dbfe441dba3be2692c3a1ada05b2d
Greg Orvik <gvorvik@gmail.com>
2018-04-18 10:02:13 -0500

    Display quotes for allquotes page

--- server/server.js
+++ server/server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
+const quotes = require('./quotes');
+
 app.get('/', (req, res) => {
     res.send('Happy Wednesday');
 });
 
+app.get('/all-quotes', (req, res) => {
+    res.send(quotes);
+});
+
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-    
+    console.log(`Listening on port ${PORT}`);  
 });
