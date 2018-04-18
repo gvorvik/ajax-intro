e684f72cb4db7c5a19528e4204f0b2b63af8873a
Greg Orvik <gvorvik@gmail.com>
2018-04-18 13:22:08 -0500

    Produce a random quote on quote page

--- server/server.js
+++ server/server.js
@@ -1,15 +1,28 @@
+//npm init -y automatically agrees to all values of a package.json file
+//express cannot send back numbers because those are reserved for statuses (e.g 404)
+//to send a number, wrap it in an object
+//request/response is 1:1 - one request gets back only one response.
+//Always remember to create a start command in the package.json
+//When downloading another project, generally we can run npm install and npm start and we're good to go
+
 const express = require('express');
+const quotes = require('./quotes');
+const randomNumber = require('./random-number');
 const app = express();
 const PORT = 5000;
 
-const quotes = require('./quotes');
-
 app.get('/', (req, res) => {
     res.send('Happy Wednesday');
+    console.log('yeah it worked.');
 });
 
 app.get('/all-quotes', (req, res) => {
     res.send(quotes);
+});
+
+app.get('/quote', (req, res) => {
+    let number = randomNumber(0, 2);
+    res.send(quotes[number]);
 });
 
 app.listen(PORT, () => {
