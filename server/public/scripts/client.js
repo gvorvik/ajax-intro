089c009ad2aa4ca2dcf7bba0fc3f6f7b026e05eb
Greg Orvik <gvorvik@gmail.com>
2018-04-18 15:55:24 -0500

    add a quote on button click

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,34 @@
+//AJAX (Asynchronous javascript and xml) is a method for connecting server side javascript with client side javascript
+
 console.log('client.js is loaded');
 
 $(document).ready(onReady);
 
 function onReady() {
     console.log('JQuery is loaded');
+
+    //the ajax method that fires off a request
+    $.ajax({
+        //type of request
+        type: 'GET',
+        //where request is going
+        url: '/all-quotes'
+    })
+    .then(function(response) {
+        //this is the promise of a response
+        console.log(response);
+    });
+
+    $('#quoteButton').on('click', getQuote);
+
+}
+
+function getQuote() {
+    $.ajax({
+        type: 'GET',
+        url: '/quote'
+    })
+    .then(function(response) {
+        $('body').append(`<p>"${response.quote}"  - ${response.author}</p>`);
+    });
 }
